5891ba10884f0a0dca1574355bd92e3b1a4d87d5
Soumya Sengupta <ssengupta@boundlessgeo.com>
2016-02-23 18:10:56 -0500

    Added support for Tomcat deployments.

--- src/main/java/com/boundlessgeo/ps/tm/TokenManagerApplication.java
+++ src/main/java/com/boundlessgeo/ps/tm/TokenManagerApplication.java
@@ -3,12 +3,20 @@ package com.boundlessgeo.ps.tm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {
 		org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class,
 		org.springframework.boot.actuate.autoconfigure.ManagementSecurityAutoConfiguration.class })
-public class TokenManagerApplication {
+public class TokenManagerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder application) {
+		return application.sources(TokenManagerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TokenManagerApplication.class, args);
